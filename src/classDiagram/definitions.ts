d7379d3073c6dfe07fff97846a1d2941f3f0ec08
Seokseong Jeon <sjeon87+fork@gmail.com>
2022-02-03 21:10:36 +0900

    fix arrow head position

--- src/classDiagram/definitions.ts
+++ src/classDiagram/definitions.ts
@@ -8,7 +8,7 @@ export function addDefinitions(
     .append("marker")
     .attr("id", "arrow")
     .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
+    .attr("refX", 0)
     .attr("refY", -0.5)
     .attr("markerWidth", 6)
     .attr("markerHeight", 6)
