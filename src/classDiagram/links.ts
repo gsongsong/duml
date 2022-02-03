3c38684e416934f28189104dee216622098d0936
Seokseong Jeon <sjeon87+fork@gmail.com>
2022-02-03 20:54:46 +0900

    adjust arrow length and position

--- src/classDiagram/links.ts
+++ src/classDiagram/links.ts
@@ -1,5 +1,6 @@
 import * as d3lib from "d3";
 import { SimulationLinkDatum, SimulationNodeDatum } from "d3";
+import { FONT_SIZE } from "./canvas";
 
 export function addLinks(
   svg: d3lib.Selection<SVGSVGElement, undefined, null, undefined>,
@@ -18,16 +19,59 @@ export function addLinks(
 /**
  * Renders an arc link
  */
- export function linkArcFunc(d: SimulationLinkDatum<SimulationNodeDatum>) {
-  const target = d.target as SimulationNodeDatum;
-  const tx = target.x ?? 0;
-  const ty = target.y ?? 0;
-  const source = d.source as SimulationNodeDatum;
-  const sx = source.x ?? 0;
-  const sy = source.y ?? 0;
-  const r = Math.hypot(tx - sx, ty - sy);
+export function linkArcFunc(d: SimulationLinkDatum<SimulationNodeDatum>) {
+  function xy(node: SimulationNodeDatum) {
+    const x = node.x ?? 0;
+    const y = node.y ?? 0;
+    return [x, y];
+  }
+
+  function nodeRatio(node: any) {
+    return node.maxRows / node.maxLength;
+  }
+
+  function offset(dx: number, dy: number, node: any) {
+    const slopeLink = dy / dx;
+    const slopeAbs = Math.abs(slopeLink);
+    const targetRatio = nodeRatio(node);
+    const [x_sign, y_sign] = [-Math.sign(dx), -Math.sign(dy)];
+    // Check link touches left/right or top/bottom side of node
+    if (slopeAbs < targetRatio) {
+      // Link touches left/right side of node
+      const tx_offset =
+        (x_sign * ((node as any).maxLength + 2) * FONT_SIZE) / 2;
+      const ty_offset =
+        (y_sign * ((node as any).maxLength + 2) * slopeAbs * FONT_SIZE) / 2;
+      return [tx_offset, ty_offset];
+    } else if (slopeAbs > targetRatio) {
+      // Link touches top/bottom side of node
+      const tx_offset =
+        (((x_sign * (node as any).maxRows) / slopeAbs) * FONT_SIZE) / 2;
+      const ty_offset = (y_sign * (node as any).maxRows * FONT_SIZE) / 2;
+      return [tx_offset, ty_offset];
+    } else {
+      const tx_offset =
+        (x_sign * ((node as any).maxLength + 2) * FONT_SIZE) / 2;
+      const ty_offset = (y_sign * (node as any).maxRows * FONT_SIZE) / 2;
+      return [tx_offset, ty_offset];
+    }
+  }
+
+  const [tx, ty] = xy(d.target as SimulationNodeDatum);
+  const [sx, sy] = xy(d.source as SimulationNodeDatum);
+
+  const dx = tx - sx;
+  const dy = ty - sy;
+
+  // Target
+  const [tx_offset, ty_offset] = offset(dx, dy, d.target);
+  // Source
+  const [sx_offset, sy_offset] = offset(dx, dy, d.source);
+
+  const r = Math.hypot(dx + tx_offset + sx_offset, dy + ty_offset + sy_offset);
+
   return `
-      M${sx},${sy}
-      A${r},${r} 0 0,1 ${tx},${ty}
+      M${sx - sx_offset},${sy - sy_offset}
+      A${r},${r} 0 0,1 ${tx + tx_offset},${ty + ty_offset}
     `;
 }
